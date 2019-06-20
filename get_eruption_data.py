05a7479963902ef2a23caece9563649c555ef412
Jillian Schleicher <jmschleicher11@gmail.com>
2019-06-20 07:15:00 -0700

    Plotting ongoing volcanoes differently from new eruptions. Need to verify that determinging ongoing vs. new eruptions is correct.

--- get_eruption_data.py
+++ get_eruption_data.py
@@ -2,28 +2,31 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import regex as re
 
-def clean_dates(row):
-    ## Make BCE years negative
-    if re.search('BCE', row):
-        row = re.split('BCE()', row)[0]
-        row = re.sub('[^\w]', '', row)
-        row = int(re.match(r'\d{4}', row)[0])
-        row = -row
-    ## Extract year, removing extra information
-    elif re.search('\d', row):
-        row = re.split('[±()]', row)[0]
-        row = re.sub('[^\w]', '', row)
-        row = int(re.match(r'\d{4}', row)[0])
-    ## If no date information, return None
-    ## Note: column converted to floats if NaN is present
+def clean_dates(date, certainty):
+    if certainty == 'Discredited':
+        return None
     else:
-        row = None
-    return row
+        ## Make BCE years negative
+        if re.search('BCE', date):
+            date = re.split('BCE()', date)[0]
+            date = re.sub('[^\w]', '', date)
+            date = int(re.match(r'\d{4}', date)[0])
+            date = -date
+        ## Extract year, removing extra information
+        elif re.search('\d', date):
+            date = re.split('[±()]', date)[0]
+            date = re.sub('[^\w]', '', date)
+            date = int(re.match(r'\d{4}', date)[0])
+        ## If no date information, return None
+        ## Note: column converted to floats if NaN is present
+        else:
+            date = None
+        return date
 
 def eruption_range(row):
 
-    start = clean_dates(row['start_date'])
-    end = clean_dates(row['stop_date'])
+    start = clean_dates(row['start_date'], row['eruption_certainty'])
+    end = clean_dates(row['stop_date'], row['eruption_certainty'])
 
     ## Use the start and end date to make a range of eruption years
     if pd.notnull(start) & pd.notnull(end):
@@ -35,11 +38,9 @@ def eruption_range(row):
 def parse_table(volc):
 
     print(volc)
-
     ## Read each volcano's html page as a soup object
     soup = BeautifulSoup(open("../Data/volcano_html_pages/"+str(volc)+".html"),
                     "html.parser")
-
     ## Identify eruption tables in file
     table = soup.find("table",
         attrs={"class":"DivTable",
@@ -71,4 +72,4 @@ def parse_table(volc):
 
 volcano_info = pd.read_csv('../Data/all_volcano_data.csv')
 all_eruptions = pd.concat([parse_table(volc) for volc in volcano_info['id']])
-all_eruptions.to_csv('../Data/all_eruption_data.csv')
+all_eruptions.to_csv('../Data/all_eruption_data.csv', index=False)
--- watching_eruptions.py
+++ watching_eruptions.py
@@ -22,7 +22,8 @@ def eruption_years(df):
     return pd.DataFrame( {"id" : np.repeat(df['id'].values,lens), 
                           "year" : np.concatenate(years)})
     
-
+## Previoualy generated data which include volcanoes with their info, and the
+    ## dates of known eruptions
 volcanoes = pd.read_csv('../Data/all_volcano_data.csv')
 eruptions = pd.read_csv('../Data/all_eruption_data.csv')
 
@@ -31,18 +32,21 @@ volcanoes = volcanoes[volcanoes['time_period'] == 'H'].reset_index()
 
 eruptions['eruption_years'] = \
     [ast.literal_eval(item) for item in eruptions['eruption_years']]
-    
-years = eruption_years(eruptions)
 
+## Get a dataframe of all eruption years, separate into new and ongoing
+## eruptions, and merge on volcanoes dataframe
+years = eruption_years(eruptions)
+years['match'] = years['year'].diff().eq(1)
 volcano_eruptions = volcanoes.merge(years, on='id', how='left')
 
 
 ## Plotting the eruptions
-year_0 = 2000
+year_0 = 1900
 
 fig=plt.figure(figsize=[6, 6])
 
-
+## Plot basemap of world and all holocene volcanoes. This will be constant in 
+## the animation
 my_map = Basemap(llcrnrlon=-180.,llcrnrlat=-80.,\
     urcrnrlon=180.,urcrnrlat=85.,\
     rsphere=(6378137.00,6356752.3142),\
@@ -56,29 +60,53 @@ x, y = my_map(np.array(volcano_eruptions['longitude']),
               np.array(volcano_eruptions['latitude']))
 ax = plt.plot(x, y, markersize=5, marker='.', linestyle='')
 
-erupting = volcano_eruptions[volcano_eruptions['year'] == year_0]
-long = np.array(erupting['longitude'])
-lat = np.array(erupting['latitude'])
-points = my_map.plot(long, lat, markersize=8, marker='^', linestyle='', 
-                     color='r')[0]
+def eruption_locs(year, df):
+    '''
+    Identify locations of new vs. erupting volcanoes to plot
+    '''
+    new_erupting = df[(df['year'] == year) & (df['match'] == 0)]
+    new_long = np.array(new_erupting['longitude'])
+    new_lat = np.array(new_erupting['latitude'])
+    
+    ongoing_erupting = df[(df['year'] == year) & (df['match'] == 1)]
+    ongoing_long = np.array(ongoing_erupting['longitude'])
+    ongoing_lat = np.array(ongoing_erupting['latitude'])
+    
+    return (new_long, new_lat, ongoing_long, ongoing_lat)
+    
+new_long, new_lat, ongoing_long, ongoing_lat = \
+                                    eruption_locs(year_0, volcano_eruptions)
+
+## Plot both new eruptions (solid markers) and ongoing eruptions (open markers)
+ongoing_points = my_map.plot(ongoing_long, ongoing_lat, markersize=5, 
+                             marker='^', linestyle='', markerfacecolor='white',
+                             markeredgecolor='red', markeredgewidth=1)[0]
+new_points = my_map.plot(new_long, new_lat, markersize=6, marker='^', 
+                     linestyle='', color='r')[0]
 
 def init():
-    points.set_data([], [])
-    return points,
+    new_points.set_data([], [])
+    ongoing_points.set_data([], [])
+    return new_points, ongoing_points,
 
 def erupting(year):
     
-    plt.title(year)
-    erupting = volcano_eruptions[volcano_eruptions['year'] == year]
-    long = np.array(erupting['longitude'])
-    lat = np.array(erupting['latitude'])
+    plt.title(year)    
+    new_long, new_lat, ongoing_long, ongoing_lat = \
+                                    eruption_locs(year, volcano_eruptions)
     
-    x, y = my_map(long, lat)
-    points.set_data(x, y)
-    return points,
+    new_x, new_y = my_map(new_long, new_lat)
+    new_points.set_data(new_x, new_y)
+    
+    ongoing_x, ongoing_y = my_map(ongoing_long, ongoing_lat)
+    ongoing_points.set_data(ongoing_x, ongoing_y)
+    
+    return new_points, ongoing_points,
 
 ani = animation.FuncAnimation(plt.gcf(), erupting, init_func=init,
                               frames=np.arange(year_0, 2020), blit=False, 
                               repeat=False)
 
 plt.show()
+ani.save(filename="../Data/figures/eruptions.mp4", fps=3, bitrate=1500, 
+         dpi=300)
